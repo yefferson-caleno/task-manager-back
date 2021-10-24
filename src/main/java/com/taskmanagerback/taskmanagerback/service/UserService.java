f65e9536929354f922468aa274b1b72d328b62b2
yefferson-caleno <yefferson.caleno@o365.unab.edu.co>
2021-10-24 09:10:54 -0500

    Añadir busqueda generar a servicios

--- src/main/java/com/taskmanagerback/taskmanagerback/service/UserService.java
+++ src/main/java/com/taskmanagerback/taskmanagerback/service/UserService.java
@@ -6,12 +6,17 @@ import com.taskmanagerback.taskmanagerback.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
 public class UserService {
     @Autowired
     private UserRepository userRepository;
+
+    public List<UserModel> findAll() {
+        return userRepository.findAll();
+    }
 
     public Optional<UserModel> findById(Long id) {
         return userRepository.findById(id);

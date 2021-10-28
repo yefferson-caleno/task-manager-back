2a3ea9cee52c4e1b7288a87733e3d7877abf3749
yefferson-caleno <yefferson.caleno@o365.unab.edu.co>
2021-10-27 22:21:55 -0500

    Configuración de proyecto en rama principal

---
+++ src/main/java/com/yefferson/taskmanagerback/TaskManagerBackApplication.java
@@ -0,0 +1,13 @@
+package com.yefferson.taskmanagerback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagerBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskManagerBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yefferson/taskmanagerback/TaskManagerBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yefferson.taskmanagerback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagerBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
